18700ac6d663a82b6be9865fc753aaaf9972e60a
Ruoris <48207031+Ruoris@users.noreply.github.com>
2020-10-02 15:00:23 +0300

    biolight client

--- IRepository.cs
+++ IRepository.cs
@@ -7,20 +7,22 @@ using System.Collections.Generic;
 
 public interface IRepository
 {
-    Task<Player> Get(Guid id);
+    Task<Player> Get(string name);
     Task<Player[]> GetAll();
-    Task<Player> Create(Player player);
-    Task<Player> Modify(Guid id, ModifiedPlayer player);
-    Task<Player> Delete(Guid id);
 
-    Task<Item> CreateItem(Guid playerId, Item item);
-    Task<Item> GetItem(Guid playerId, Guid itemId);
-    Task<Item[]> GetAllItems(Guid playerId);
-    Task<Item> UpdateItem(Guid playerId, Guid itemId, ModifiedItem item);
-    Task<Item> DeleteItem(Guid playerId, Guid itemId);
-    Task<List<Player>> Ranges(int seekethNumber);
+    Task<List<Player>> GetAllFromPlayer(string name);
+    Task<Player> Create(Player player);
+    // Task<Player> Modify(Guid id, ModifiedPlayer player);
+    // Task<Player> Delete(Guid id);
+
+    // Task<Item> CreateItem(Guid playerId, Item item);
+    // Task<Item> GetItem(Guid playerId, Guid itemId);
+    // Task<Item[]> GetAllItems(Guid playerId);
+    // Task<Item> UpdateItem(Guid playerId, Guid itemId, ModifiedItem item);
+    // Task<Item> DeleteItem(Guid playerId, Guid itemId);
+    // Task<List<Player>> Ranges(int seekethNumber);
     Task<List<Player>> Sorting();
-    Task<Player> Skeletor(string name);
-    Task<List<Player>> Subzero(ItemType itemtype);
-    Task<Player> Skeletor2(Guid id);
+    // Task<Player> Skeletor(string name);
+    // Task<List<Player>> Subzero(ItemType itemtype);
+    // Task<Player> Skeletor2(Guid id);
 }
--- Player.cs
+++ Player.cs
@@ -3,11 +3,22 @@ using System;
 using System.Collections.Generic;
 public class Player
 {
+
+
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public int Score { get; set; }
-    public int Level { get; set; }
-    public bool IsBanned { get; set; }
+    public string namer { get; set; }
+    public string UserName { get; set; }
+    public float Score { get; set; }
+    public int EnemiesKilled { get; set; }
+    public string Time { get; set; }
+
+    public int FloorsCleared { get; set; }
+
+    // public Guid Id { get; set; }
+    // public string Name { get; set; }
+    // public int Score { get; set; }
+    // public int Level { get; set; }
+    // public bool IsBanned { get; set; }
     public DateTime CreationTime { get; set; }
-    public List<Item> itemList { get; set; }
+    // public List<Item> itemList { get; set; }
 }
